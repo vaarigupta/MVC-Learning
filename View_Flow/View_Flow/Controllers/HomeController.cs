28581491829eb4656b74bed7e837690efed09c0e
vaarigupta <vaarigupta24@gmail.com>
2020-11-24 16:05:10 +0530

    2 projects added

---
+++ View_Flow/View_Flow/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using View_Flow.Models;
+
+namespace View_Flow.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var data = GetEmployee();
+            return View(data);
+        }
+       
+        private Employee GetEmployee()
+        {
+            return new Employee()
+            {
+                id = 1,
+                name = "Vaari",
+                designation = "Software Developer"
+            };
+        }
+      
+        
+    }
+}
